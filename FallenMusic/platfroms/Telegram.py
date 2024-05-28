9a4df8e7c45cbc106c02a1d595a4f75cabfc983b
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-29 01:56:25 +0300

    Update Telegram.py

--- FallenMusic/platfroms/Telegram.py
+++ FallenMusic/platfroms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from ZeMusic import app
-from ZeMusic.utils.formatters import (
+from FallenMusic import app
+from FallenMusic.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
