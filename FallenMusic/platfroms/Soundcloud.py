ebfb3807816f7c330295731262128801e438fe72
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-29 01:54:45 +0300

    Update Soundcloud.py

--- FallenMusic/platfroms/Soundcloud.py
+++ FallenMusic/platfroms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from ZeMusic.utils.formatters import seconds_to_min
+from FallenMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
