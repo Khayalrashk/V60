f045eb0d568a51da8b06ba0671a62be74f167c74
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-28 19:40:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -17,9 +17,13 @@ PING_IMG = getenv("PING_IMG", "https://graph.org/file/325aa1aa99e917235149e.jpg"
 START_IMG = getenv("START_IMG", "https://graph.org/file/325aa1aa99e917235149e.jpg")
 
 SESSION = getenv("SESSION", None)
+# Fill this variable if your upstream repository is private
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/rr_r_v")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/rr_r_v")
+CHANNEL_NAME = getenv("CHANNEL_NAME", "• 𝐒𝐨𝐮𝐫𝐜𝐞 𝐋𝐚𝐫𝐢𝐧 🎧")
+CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/sourcelarin")
+
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SOURCELARIN")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SOURCELARIN")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5190136458").split()))
 
